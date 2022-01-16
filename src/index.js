a8250155612e1e15059f8e3d3b0375585ef05b2f
ponprabhakar <ponprabhakarpg@gmail.com>
2022-01-16 23:09:44 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import { useState } from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//This fuction uses state
+function MyForm() {
+  const [name, setName] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const handleChange = (event) => {
+    ReactDOM.render(<Message msg={name}/>, document.getElementById('root1'));
+  }
+
+  return (
+    <form onChange={handleChange}>
+      <label>Enter your name:
+        <input 
+          type="text" 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+
+//This function uses Props
+function Message(props){
+    return <div><h3>Keyboard Changes</h3><h5>{props.msg}</h5></div>;
+}
+
+ReactDOM.render(<MyForm />, document.getElementById('root'));
